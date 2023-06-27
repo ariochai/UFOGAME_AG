d7d7f51ff4a679d49c070624697930a24d175522
Anna Gądek <ariochaimail@gmail.com>
2023-06-27 18:37:04 +0200

    added Sound and AudioManager

---
+++ Assets/Sounds/Sounds.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public string name;
+    public AudioClip audioClip;
+
+
+}
